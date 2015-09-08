d38d23c23f5ce70aa03f568562a0cdbbec6e1526
eduaglz <eduaenrique@gmail.com>
2015-09-08 16:24:25 +0000

    Original Image added

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ using namespace cv;
 char key;
 int main()
 {
-    cvNamedWindow("Camera_Output", 1);    //Create window
-    VideoCapture camera(0);
+    cvNamedWindow("Original", 1);    //Create window
+    cvNamedWindow("Threshold", 1);
+	VideoCapture camera(0);
     camera.set(CV_CAP_PROP_FRAME_WIDTH,320);
     camera.set(CV_CAP_PROP_FRAME_HEIGHT,240);
      if(!camera.isOpened())
@@ -18,12 +19,15 @@ int main()
       }
     while(1){ //Create infinte loop for live streaming
         Mat frame;
-	Mat threshold(320,240,CV_8U);
-        camera >> frame;
+	Mat threshold(320,240,CV_8UC1,255);
+	camera >> frame;
+	inRange(frame,Scalar(0,0,0), Scalar(255,255,20), threshold);
+        //camera >> frame;
         cvtColor(frame, frame, CV_RGB2HSV);
-	threshold(frame, frame, Scalar(0,0,0), Scalar(255,255,50), threshold);
-	imshow("Camera_Output", threshold);   //Show image frames on created window
-        key = cvWaitKey(10);     //Capture Keyboard stroke
+	//threshold(frame, frame, Scalar(0,0,0), Scalar(255,255,50), threshold);
+	imshow("Threshold", threshold);   //Show image frames on created window
+        imshow("Original", frame);
+	key = cvWaitKey(10);     //Capture Keyboard stroke
         if (char(key) == 27){
             break;      //If you hit ESC key loop will break.
         }
